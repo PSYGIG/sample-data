27faeb4e52861abed1b27c50ec54d8ac8a9da198
tngan <tonynwk919@gmail.com>
2021-01-21 16:55:17 +0800

    Restructure to have 1.7 and 2.0 sample output

---
+++ potree/1.7/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 6619136,
+    "boundingBox": {
+        "lx": -134.0,
+        "ly": -148.0,
+        "lz": -15.0,
+        "ux": 169.0,
+        "uy": 155.0,
+        "uz": 288.0
+    },
+    "tightBoundingBox": {
+        "lx": -134.0,
+        "ly": -148.0,
+        "lz": -15.0,
+        "ux": 127.0,
+        "uy": 155.0,
+        "uz": 38.0
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 2.6240570545196535,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
